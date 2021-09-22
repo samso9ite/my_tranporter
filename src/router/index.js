19d6940045dbe9b54629fa868d1289ab4967adc8
Samson Ajayi <43522374+samso9ite@users.noreply.github.com>
2021-09-22 10:28:28 +0100

    Authentication Updated

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,14 @@ const routes = [
   {
   path: '/',
   name: 'Dashboard',
-  component: () => import('../views/UserApp/UserDashboard.vue')
+  component: () => import('../views/UserApp/UserDashboard.vue'),
+  meta: { requiresAuth: true }
   },
   {
     path:'/user-signup',
     name: 'Signup',
-    component : () => import('../views/UserApp/Auth/Signup.vue')
+    component : () => import('../views/UserApp/Auth/Signup.vue'),
+   
   },
   {
     path: '/login',
@@ -24,7 +26,8 @@ const routes = [
   {
     path: '/order-history',
     name: 'OrderHistory',
-    component: () => import('../views/UserApp/OrderHistory.vue')
+    component: () => import('../views/UserApp/OrderHistory.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/validate-otp',
@@ -39,29 +42,34 @@ const routes = [
   {
     path:'/order-history',
     name: 'OrderHistory',
-    component: () => import('../views/UserApp/OrderHistory.vue')
+    component: () => import('../views/UserApp/OrderHistory.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path:'/wallet',
     name: 'Wallet',
-    component: () => import('../views/UserApp/Wallet.vue')
+    component: () => import('../views/UserApp/Wallet.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path:'/update-profile',
     name: 'Profile',
-    component: () => import('../views/UserApp/Profile.vue')
+    component: () => import('../views/UserApp/Profile.vue'),
+    meta: { requiresAuth: true }
   },
   {
     path: '/paystack/:paystack_url',
     name: 'Paystack',
     component: () => import('../views/UserApp/PaymentPage.vue'),
-    props:true
+    props:true,
+    meta: { requiresAuth: true }
   },
 
   {
     path: '/notifications',
     name: 'Notifications',
-    component: () => import('../views/UserApp/Notification.vue')
+    component: () => import('../views/UserApp/Notification.vue'),
+    meta: { requiresAuth: true }
   },
 
   // Merchant Routers
@@ -71,11 +79,11 @@ const routes = [
 //   name: 'Registera',
 //   component: () => import('../views/MerchantApp/Auth/Register.vue')
 // },
-{
-  path:'/merchant-login',
-  naame:'Login',
-  component: () => import('../views/MerchantApp/Auth/Login.vue')
-},
+// {
+//   path:'/merchant-login',
+//   naame:'Login',
+//   component: () => import('../views/MerchantApp/Auth/Login.vue')
+// },
 {
   path:'/merchant-otp-validation',
   name: 'ActivateAccount',
@@ -91,6 +99,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (window.localStorage.getItem('isAuthenticated') === 'true') {
+      next()
+    } else {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
 })
 
 export default router
